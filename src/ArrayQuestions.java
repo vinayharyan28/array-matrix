d85ccc8734d7c0ea78cc5e5c89798b89aefade3b
vinayharyan28 <vinayharyan28@gmail.com>
2023-08-29 22:02:35 +0530

    merge two array

--- src/ArrayQuestions.java
+++ src/ArrayQuestions.java
@@ -50,17 +50,39 @@ public class ArrayQuestions {
         return -1;
     }
 
+    public static int[] mergeTwoSortedArrays(int[] arr1, int[] arr2){
+        int[] arr3 = new int[arr1.length + arr2.length];
+        int arr1Index=0, arr2Index=0, arr3Index=0;
+        while (arr1Index < arr1.length && arr2Index < arr2.length){
+            if (arr1[arr1Index] < arr2[arr2Index]){
+                arr3[arr3Index++] = arr1[arr1Index++];
+            }else{
+                arr3[arr3Index++] = arr2[arr2Index++];
+            }
+        }
+
+        while (arr1Index < arr1.length){
+            arr3[arr3Index++] = arr1[arr1Index++];
+        }
+
+        while (arr2Index < arr2.length){
+            arr3[arr3Index++] = arr2[arr2Index++];
+        }
+
+        return arr3;
+    }
+
     public static void main(String[] args){
         int[] arr = {3, 5, 4, 1, 9};
         int[] arr1 = {22, 14, 8, 17, 35, 3};
         int[] result = minimumMaximum(arr1);
         kthSmallestElement(arr, 3);
         System.out.println(Arrays.toString(result));
-        int[] arr2 = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         int[] arr3 = {2, 1, 0, 3};
+        int[] arr10 = { 5, 8, 9};
+        int[] arr11 = {4, 7, 8};
         System.out.println("Minimum jumps: " + minimumNumberOfJumps(arr3));
-
-
+        System.out.println("Merge two sorted array: " + Arrays.toString(mergeTwoSortedArrays(arr10, arr11)));
     }
 
 }
